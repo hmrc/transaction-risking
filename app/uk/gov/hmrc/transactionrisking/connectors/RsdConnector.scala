91c072ff03568aececc20b17c074792ab0997787
HG-Accenture <183600681+HG-Accenture@users.noreply.github.com>
2026-06-01 13:49:54 +0100

    Added licensing and removed unused code

--- app/uk/gov/hmrc/transactionrisking/connectors/RsdConnector.scala
+++ app/uk/gov/hmrc/transactionrisking/connectors/RsdConnector.scala
@@ -1,59 +1,5 @@
-package uk.gov.hmrc.transactionrisking.connectors
-//
-//import play.api.Logging
-//import play.api.http.Status.{BAD_REQUEST, INTERNAL_SERVER_ERROR, NO_CONTENT}
-//import play.api.libs.json.Json
-//import play.api.libs.ws.writeableOf_JsValue
-//import uk.gov.hmrc.http.HttpReads.Implicits.readRaw
-//import uk.gov.hmrc.http.client.HttpClientV2
-//import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse, StringContextOps}
-//import uk.gov.hmrc.transactionrisking.config.AppConfig
-//import uk.gov.hmrc.transactionrisking.models.request.rds.RdsRequest
-//import uk.gov.hmrc.transactionrisking.models.response.rds.ErrorMessages
-//
-//import javax.inject.{Inject, Singleton}
-//import scala.concurrent.{ExecutionContext, Future}
-//import scala.util.Try
-//
-//@Singleton
-//class RDSConnector @Inject()(
-//                              httpClient: HttpClientV2,
-//                              appConfig: AppConfig
-//                            )(implicit ec: ExecutionContext) extends Logging {
-//
-//  // assumes AppConfig has rdsBaseUrl, e.g. "http://localhost:8082"
-//  private val receiveAndStoreUrl = url"${appConfig.rdsServiceBaseUrl}/receive-and-store"
-//
-//  def receiveAndStore(request: RdsRequest, correlationId: Option[String])(implicit hc: HeaderCarrier): Future[Unit] = {
-//    val extraHeaders = correlationId.map(id => Seq("CorrelationId" -> id)).getOrElse(Seq.empty)
-//
-//    httpClient
-//      .post(receiveAndStoreUrl)
-//      .setHeader(extraHeaders*)
-//      .withBody(Json.toJson(request))
-//      .execute[HttpResponse]
-//      .map { response =>
-//        response.status match {
-//          case NO_CONTENT => ()
-//          case BAD_REQUEST =>
-//            val err = Try(Json.parse(response.body).as[ErrorMessages]).toOption
-//            logger.warn(s"RDS 400 response: ${err.getOrElse(response.body)}")
-//            throw new RuntimeException(s"RDS validation failed: ${err.map(_.error).getOrElse("Bad Request")}")
-//          case INTERNAL_SERVER_ERROR =>
-//            logger.error(s"RDS 500 response: ${response.body}")
-//            throw new RuntimeException("RDS internal server error")
-//          case status =>
-//            logger.warn(s"Unexpected RDS response status=$status body=${response.body}")
-//            throw new RuntimeException(s"Unexpected RDS status: $status")
-//        }
-//      }
-//  }
-//}
-
-
-
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -67,6 +13,9 @@ package uk.gov.hmrc.transactionrisking.connectors
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package uk.gov.hmrc.transactionrisking.connectors
+
 
 import play.api.Logging
 import play.api.http.Status.NO_CONTENT
--- app/uk/gov/hmrc/transactionrisking/models/response/rsd/RsdResponse.scala
+++ app/uk/gov/hmrc/transactionrisking/models/response/rsd/RsdResponse.scala
@@ -1,39 +1,18 @@
-//package uk.gov.hmrc.transactionrisking.models.response
-//
-//import play.api.libs.json.{Json, OFormat}
-//
-//
-//case class RDSResponse(
-//                            riskScore: Double,
-//                            riskCorrelationId: String,
-//                            reasons: Seq[String]
-//                      )
-//
-//object RDSResponse:
-//  implicit val reads: OFormat[RDSResponse] = Json.format[RDSResponse]
-
-
-
-
-
-
-//package uk.gov.hmrc.transactionrisking.models.response.rds
-//
-//import play.api.libs.json.{Json, OFormat}
-//
-//case class ErrorMessage(error: String)
-//object ErrorMessage:
-//  implicit val format: OFormat[ErrorMessage] = Json.format[ErrorMessage]
-//
-//case class ErrorMessages(
-//                          timestamp: String,
-//                          status: Int,
-//                          error: String,
-//                          path: String,
-//                          errors: Seq[ErrorMessage] = Seq.empty
-//                        )
-//object ErrorMessages:
-//  implicit val format: OFormat[ErrorMessages] = Json.format[ErrorMessages]
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 
 
